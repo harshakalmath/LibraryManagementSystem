d317f00183097c44d8a4e2477352a6b5bb0a4493
harshakalmath <harsha17.kalmath@gmail.com>
2023-05-05 20:48:06 -0600

    added while loop for menu

--- src/main/java/com/application/frontend/Frontend.java
+++ src/main/java/com/application/frontend/Frontend.java
@@ -17,46 +17,67 @@ public class Frontend {
 
         String type = new String();
         System.out.println("Hi! Welcome to the library..");
-        System.out.println("Choose if you want to login/register:");
-        System.out.println("1.Login");
-        System.out.println("2.Register");
-        int op = sc.nextInt();
-        switch (op) {
-            case 1: type = libraryServices.login();
-                break;
-            case 2: libraryServices.registerNewMember();
-                break;
-        }
-        if(op==1)
-            if(type.equalsIgnoreCase("librarian")) {
-                System.out.println("Add book to database");
-                libraryServices.addBook();
+        int op = 0;
+        while(op!=3) {
+            System.out.println("Choose if you want to login/register:");
+            System.out.println("1.Login");
+            System.out.println("2.Register");
+            System.out.println("3.Press 3 to exit menu");
+            op = sc.nextInt();
+
+            switch (op) {
+                case 1:
+                    type = libraryServices.login();
+                    break;
+                case 2:
+                    libraryServices.registerNewMember();
+                    break;
+                case 3:
+                    break;
             }
-            else {
-                System.out.println("Choose from the following:");
-                System.out.println("1.Search catalog by name");
-                System.out.println("2.Search catalog by author");
-                System.out.println("3.Search catalog by genre");
-                System.out.println("4.Issue book");
-                System.out.println("5.Return book");
-                int option = sc.nextInt();
-                switch (option) {
-                    case 1: libraryServices.searchByName();
-                        break;
-                    case 2: libraryServices.searchByAuthor();
-                        break;
-                    case 3: libraryServices.searchByGenre();
-                        break;
-                    case 4: libraryServices.issueBook();
-                        break;
-                    case 5: libraryServices.returnBook();
-                        break;
+            if (op == 1)
+                if (type.equalsIgnoreCase("librarian")) {
+                    System.out.println("Add book to database");
+                    libraryServices.addBook();
+                } else {
+                    int option=0;
+                    while (option!=6) {
+                        System.out.println("Choose from the following:");
+                        System.out.println("1.Search catalog by name");
+                        System.out.println("2.Search catalog by author");
+                        System.out.println("3.Search catalog by genre");
+                        System.out.println("4.Issue book");
+                        System.out.println("5.Return book");
+                        System.out.println("6.Press 6 to exit menu:");
+
+                        option = sc.nextInt();
+
+                        switch (option) {
+                            case 1:
+                                libraryServices.searchByName();
+                                break;
+                            case 2:
+                                libraryServices.searchByAuthor();
+                                break;
+                            case 3:
+                                libraryServices.searchByGenre();
+                                break;
+                            case 4:
+                                libraryServices.issueBook();
+                                break;
+                            case 5:
+                                libraryServices.returnBook();
+                                break;
+                            case 6:
+                                break;
+
+                        }
+
+                    }
+
 
                 }
-
-
-
-            }
+        }
 
 
 
